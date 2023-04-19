cda4a87440615128b3db804f2e91b79f652ff894
techguy-sudo <robertball54321@gmail.com>
2023-04-19 22:37:35 +0100

    1 STAGE HELLO WORLD

---
+++ modern_website/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div classname=" bg-primary w-full overflow-hidden">
+      <h1>HELLO, WORLD!!</h1>
+
+
+    </div>
+  )
+}
+
+export default App
